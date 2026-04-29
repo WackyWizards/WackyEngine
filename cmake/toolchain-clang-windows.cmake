38208d6e1a9afe352cc41131cea19943cfc05378
kellie <kellie@wackywizards.org>
2026-04-29 20:36:10 +0300

    Compile with clang, add cmake builds

---
+++ cmake/toolchain-clang-windows.cmake
@@ -0,0 +1,5 @@
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang++)
+set(CMAKE_C_COMPILER_TARGET x86_64-pc-windows-msvc)
+set(CMAKE_CXX_COMPILER_TARGET x86_64-pc-windows-msvc)
+set(CMAKE_LINKER lld-link)
